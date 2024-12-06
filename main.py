d9ec0f851c210b367a603344dddfa084f8ee8244
Salma_ Abdelaleem <se9711748@gmail.com>
2024-12-06 17:23:18 +0200

    deleted model

---
+++ main.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+import pandas as pd
+
+app = FastAPI()
+
+# تحميل النموذج
+model = joblib.load("RF_model_compressed.joblib")
+
+label_names = {
+    1: 'Walking',
+    3: 'Shuffling',
+    4: 'Stairs (Ascending)',
+    5: 'Stairs (Descending)',
+    6: 'Standing',
+    7: 'Sitting',
+    8: 'Lying'
+}
+
+class InputData(BaseModel):
+    back_x: float
+    back_y: float
+    back_z: float
+    thigh_x: float
+    thigh_y: float
+    thigh_z: float
+
+@app.post("/predict")
+def predict(data: InputData):
+    # تحويل البيانات إلى DataFrame
+    input_df = pd.DataFrame([data.dict()])
+    # عمل التنبؤ
+    prediction = model.predict(input_df)
+    
+    # action = " "
+    # if prediction[0] != 8:
+    #     action = "Not_Fall"
+    # else:
+    #     action = "Fall"
+    return {"prediction": label_names[prediction[0]]}
+
+
